a53e334fe9d94e23b0af15ece00cb753f97ecebf
chawki benchehida <chawki27000@hotmail.com>
2016-08-19 13:52:48 +0100

    add a function to convert to timestamp

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.get('/:value', function (req, res) {
 
-    
+    console.log('REQUEST : '+req.params.value)
     // spliting paramters
     var spliting = req.params.value.split(',')
 
@@ -18,7 +18,12 @@ app.get('/:value', function (req, res) {
     var mounth = transform.mounthToNum(mounth_day[0])
     var year = parseInt(spliting[1])
 
-    console.log('The official date : '+year+' - '+mounth+' - '+day)
+    //convert to unix timestamp
+    var date_str = year+'.'+mounth+'.'+day
+    console.log('date formater : '+date_str)
+    var unixtime = new Date(date_str).getTime() / 1000
+
+    console.log('unixtime : '+unixtime)
 
 });
 
